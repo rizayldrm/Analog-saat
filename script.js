6b4e50a6ad937dfdac5e0e22d24ea1c5513c2db5
rizayldrm <rizayildirim126@gmail.com>
2023-10-23 14:23:33 +0300

    #

--- script.js
+++ script.js
@@ -15,26 +15,28 @@ setInterval(() => {
 
 //DIGITAL CLOCK
 
-let hours = document.getElementById('hour')
-let minutes = document.getElementById('minutes')
-let seconds = document.getElementById('seconds')
-let ampm = document.getElementById('ampm')
+setInterval(() => {
+    let hours = document.getElementById('hour')
+    let minutes = document.getElementById('minutes')
+    let seconds = document.getElementById('seconds')
+    let ampm = document.getElementById('ampm')
 
-let h = new Date().getHours()
-let m = new Date().getMinutes()
-let s = new Date().getSeconds()
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
 
-let am = h>= 12 ? 'PM' : 'PM'
+    let am = h >= 12 ? 'PM' : 'PM'
 
-if(h > 12){
-    h= h-12
-}
+    if (h > 12) {
+        h = h - 12
+    }
 
-h = (h < 10) ? '0' + h : h
-m = (m < 10) ? '0' + m : m
-s = (s < 10) ? '0' + s : s
+    h = (h < 10) ? '0' + h : h
+    m = (m < 10) ? '0' + m : m
+    s = (s < 10) ? '0' + s : s
 
-hours.innerHTML = h
-minutes.innerHTML = m
-seconds.innerHTML = s
-ampm.innerHTML = am
+    hours.innerHTML = h
+    minutes.innerHTML = m
+    seconds.innerHTML = s
+    ampm.innerHTML = am
+}, 1000);
